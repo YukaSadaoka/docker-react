ff7eb3465a4f4ad0118e9467be494e564270f64a
YukaSadaoka <yukasadaoka@outlook.com>
2020-12-17 14:23:12 -0500

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello There!!
+          This is a test app for CI/CD
         </p>
         <a
           className="App-link"
